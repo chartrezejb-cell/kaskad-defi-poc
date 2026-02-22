8fb3c6c9c4ce1617fb9aaa10ed19b8725708ab4e
chartrezejb-cell <chartrezejb@gmail.com>
2026-02-22 18:39:32 +0100

    Change pool empty check to use totalSupply threshold

--- frontend/src/hooks/useLiquidity.ts
+++ frontend/src/hooks/useLiquidity.ts
@@ -72,7 +72,7 @@ export function useLiquidity(signer: ethers.Signer | null, address: string | nul
     t.address === "NATIVE" ? TOKENS.kaWIKAS.address : t.address;
 
   const isPoolEmpty = (poolInfo: PoolInfo | null) =>
-  !poolInfo || ethers.utils.parseEther(poolInfo.totalSupply).eq(0);
+  !poolInfo || ethers.utils.parseEther(poolInfo.totalSupply).lte(ethers.BigNumber.from(1000));
 
   useEffect(() => {
     const fetchPool = async () => {
